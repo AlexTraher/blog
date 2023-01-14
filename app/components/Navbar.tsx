978df39c723b14963c7e1b75fb4b2f7ae915460a
Alex Traher <alex.traher@gmail.com>
2023-01-14 13:35:38 +0000

    bump versions and re-do shrinking logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,60 +1,59 @@
 import { NavLink, useLocation } from "@remix-run/react";
-import { VFC } from "react";
+import { FC, useState } from "react";
 
 interface NavbarProps {}
 
 const isHome = (pathname: string) => pathname === '/';
 
-const activeUnderline = (classNames: string) => ({ isActive }: { isActive: boolean }) => `${classNames} ${isActive ? 'sm:underline ' : ''}`;
+const activeUnderline = (classNames: string) => ({ isActive }: { isActive: boolean }) => `${classNames} ${isActive ? 'sm:underline font-semibold' : ''}`;
 
-const Navbar: VFC<NavbarProps> = () => {
+const Navbar: FC<NavbarProps> = () => {
   const { pathname } = useLocation();
-
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="
-      sticky top-0 py-2 sm:py-4 dark:bg-gait-blue-dark bg-gait-green-light z-10 flex flex-wrap 
-      after:content-[''] after:w-[80%] after:mx-4 after:border-b-2 after:border-black after:dark:border-gait-green-dark after:p-b-2 after:sm:border-0
+      sticky top-0 py-4 dark:bg-gait-blue-dark bg-gait-green-light z-10 flex px-6 flex-col sm:flex-row
     ">
-      <div className="bg-gait-light dark:bg-gait-dark bg-contain bg-no-repeat [background-position:20px] flex-1 sm:hidden h-[60px]"></div>
-      <input id="menuToggle" type="checkbox" className="block opacity-0 sm:hidden" />
-      <label htmlFor="menuToggle" className="block cursor-pointer self-center sm:hidden"></label>
-      <ul className="mt-4 justify-evenly collapsible-menu flex-col sm:flex-row sm:flex flex-shrink-0 flex-grow-0 flex-[100%]">
-        <li className="flex-shrink-0 min-w-[100px] text-center">
+      {/* TODO<a className="hidden md:block sm:underline-offset-8 hover:underline" href="mailto:hello@gait.dev">hello@gait.dev</a> */}
+      <div className="self-center flex justify-between sm:justify-start w-[100%]">
+        <NavLink to="/" className="sm:underline-offset-8 sm:hover:underline">
+          <div className={`
+            w-[151px]
+            h-[45px]
+            md:ml-[-2px]
+            bg-contain
+            bg-no-repeat
+            md:bg-center
+            ${isHome(pathname) ? 
+              `motion-safe:md:bg-none
+              motion-reduce:md:bg-gait-software-light
+              motion-reduce:md:dark:bg-gait-software-dark
+              bg-gait-i-light dark:bg-gait-i-dark
+              `
+              :
+              `md:bg-gait-software-light md:dark:bg-gait-software-dark
+              bg-gait-i-light dark:bg-gait-i-dark`
+            }
+          `}></div>
+        </NavLink>
+        <input id="menuToggle" type="checkbox" className="block opacity-0 sm:hidden" onChange={({ target }) => setMenuOpen(target.checked)}/>
+        <label htmlFor="menuToggle" className="block cursor-pointer self-center sm:hidden justify-self-end"></label>
+      </div>
+      <ul className={`mt-4 collapsible-menu flex-col sm:flex-row sm:flex flex-shrink-1 flex-grow-1 flex-[100%] justify-end ${menuOpen && 'collapsible-menu-open'}`}>
+        <li className="flex-shrink-0 flex-grow-0 min-w-[120px] text-center">
         <NavLink to="/" className={activeUnderline("sm:underline-offset-8 hover:underline uppercase")}>Home</NavLink>
         </li>
-        <li className="flex-shrink-0 min-w-[100px] text-center">
+        <li className="flex-shrink-0 flex-grow-0 min-w-[120px] text-center">
         <NavLink to="/about" className={activeUnderline("sm:underline-offset-8 sm:hover:underline uppercase")}>About</NavLink>
         </li>
-        <li className="flex-shrink-0 min-w-[100px] text-center hidden sm:block">
-          <NavLink to="/" className="sm:underline-offset-8 sm:hover:underline">
-            <div className={`
-              w-[151px]
-              h-[45px]
-              mt-[-1rem]
-              md:ml-[-2px]
-              bg-contain
-              bg-no-repeat
-              bg-center
-              ${isHome(pathname) ? 
-                `motion-safe:md:bg-none
-                motion-reduce:md:bg-gait-software-light
-                motion-reduce:md:dark:bg-gait-software-dark
-                bg-gait-i-light dark:bg-gait-i-dark
-                `
-                :
-                `md:bg-gait-software-light md:dark:bg-gait-software-dark
-                bg-gait-i-light dark:bg-gait-i-dark`
-              }
-            `}></div>
-          </NavLink>
-        </li>
-        <li className="flex-shrink-0 min-w-[100px] text-center">
+        <li className="flex-shrink-0 flex-grow-0 min-w-[120px] text-center">
         <NavLink to="/services" className={activeUnderline("sm:underline-offset-8 sm:hover:underline uppercase")}>Services</NavLink>
         </li>
-        <li className="flex-shrink-0 min-w-[100px] text-center">
+        <li className="flex-shrink-0 flex-grow-0 min-w-[120px] text-center">
         <NavLink to="/get-in-touch" className={activeUnderline("sm:underline-offset-8 sm:hover:underline uppercase")}>Get in touch</NavLink>
         </li>
       </ul>
+      
     </nav>
   )
 }
--- app/components/ShrinkableLogo.tsx
+++ app/components/ShrinkableLogo.tsx
@@ -7,6 +7,9 @@ const RATIO = 3.34 as const;
 const END_WIDTH_PX = 151;
 const END_HEIGHT_PX = 45;
 
+// To help reduce flicker on first load
+const INITIAL_INTERSECTION = 0.14595851639233237 as const;
+
 const getThresholds = (stepCount: number) => {
   const step = 1 / stepCount;
   let i = 0 - step;
@@ -21,7 +24,7 @@ function easeInOutCubic(t: number, b: number, c: number, d: number) {
 
 const useScrollSize: useScrollSize = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [multiplier, setMultiplier] = useState(0);
+  const [multiplier, setMultiplier] = useState<number>(INITIAL_INTERSECTION);
   const [prefersReduceMotion, setPrefersReduceMotion] = useState(() => {
     if (typeof window !== 'undefined') {
       return window?.matchMedia("(prefers-reduced-motion: reduce)")?.matches
@@ -80,7 +83,13 @@ const getStyle = (multiplier: number) => {
     "--diff": "calc(var(--startWidth) - var(--endWidth))",
     "--minus": "calc(var(--diff) * var(--multiplier))",
     "--width": `calc(var(--startWidth) - var(--minus))`,
+    "--startMargin": "calc((100vw - min(var(--startWidth), 800px)) / 2)",
+    "--endMargin": "0px",
+    "--diffMargin": "calc(var(--startMargin) - var(--endMargin))",
+    "--minusMargin": "calc(var(--diffMargin) * var(--multiplier))",
+    "--margin": "calc(var(--startMargin) - var(--minusMargin))",
     width: "var(--width)",
+    marginLeft: "var(--margin)",
     maxWidth: "800px",
     height: `calc(var(--width) / ${RATIO})`,
     maxHeight: `${800 / RATIO}px`,
@@ -91,13 +100,13 @@ const getStyle = (multiplier: number) => {
 
 const ShrinkableLogo = () => {
   const [multiplier, ref] = useScrollSize();
-
+  debugger;
   return (
     <>
-      <section className="justify-center motion-safe:sticky top-0 z-20 p-4 w-[200px] overflow-visible m-auto hidden md:flex">
+      <section className="justify-start motion-safe:sticky top-0 z-20 p-4 w-[200px] overflow-visible hidden md:flex ml-[-10px]">
         <div
           style={getStyle(multiplier)}
-          className="bg-gait-software-light dark:bg-gait-software-dark bg-contain bg-no-repeat flex-shrink-0"></div>
+          className="bg-gait-software-light dark:bg-gait-software-dark bg-contain bg-no-repeat flex-shrink-0 max-w-[800px]"></div>
       </section>
       <div className="h-[20vh] w-[1px] absolute top-[-10px] opacity-0" ref={ref} aria-hidden="true"></div>
     </>
